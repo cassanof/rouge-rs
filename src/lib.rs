2e5e3e9a780663c32f69a644272c7c61e506670a
cassanof <federico.cassano@federico.codes>
2023-11-14 11:56:45 -0500

    starting stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,23 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use tokenizer::Token;
+pub mod rouge;
+pub mod tokenizer;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn lcs_table(refe: &[Token], can: &[Token]) -> Vec<Vec<usize>> {
+    let rows = refe.len();
+    let cols = can.len();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    // Initialize the LCS table with zeros
+    let mut lcs_table = vec![vec![0; cols + 1]; rows + 1];
+
+    for i in 1..=rows {
+        for j in 1..=cols {
+            if refe[i - 1] == can[j - 1] {
+                lcs_table[i][j] = lcs_table[i - 1][j - 1] + 1;
+            } else {
+                lcs_table[i][j] = std::cmp::max(lcs_table[i - 1][j], lcs_table[i][j - 1]);
+            }
+        }
     }
+
+    lcs_table
 }
