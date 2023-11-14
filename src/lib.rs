71e600a283d2f6f8659d599a45a8db69316db327
cassanof <federico.cassano@federico.codes>
2023-11-14 16:29:59 -0500

    wrappers

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,26 @@ pub mod rouge;
 pub mod tokenizer;
 
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+pub fn rouge_l_dedup(solutions: Vec<&str>, threshold: f64) -> Vec<&str> {
+    let scorer = rouge::RougeLScorer::default();
+    dedup::rouge_dedup(&scorer, &solutions, threshold)
+}
+
+#[pyfunction]
+pub fn rouge_l_grouped_dedup(
+    solutions: Vec<&str>,
+    threshold: f64,
+    dedup_prob: f64,
+    max_group_size: usize,
+) -> Vec<&str> {
+    dedup::rouge_l_grouped_dedup(&solutions, threshold, dedup_prob, max_group_size)
 }
 
 #[pymodule]
 fn rouge_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<rouge::RougeLScorer>()?;
     m.add_class::<rouge::Score>()?;
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(rouge_l_grouped_dedup, m)?)?;
+    m.add_function(wrap_pyfunction!(rouge_l_dedup, m)?)?;
     Ok(())
 }
