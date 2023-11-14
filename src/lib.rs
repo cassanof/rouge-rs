8e8744fe642ca046a90c3ddba732c0b9e74bff93
cassanof <federico.cassano@federico.codes>
2023-11-14 16:11:36 -0500

    boilerplate

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+use pyo3::prelude::*;
+
 pub mod dedup;
 pub mod rouge;
 pub mod tokenizer;
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn rouge_rs(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    Ok(())
+}
